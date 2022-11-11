a9f33d72ec9f73bbfcfaae448e22536a7d57c227
Pedro Antônio <pedroantonio088@gmail.com>
2022-11-10 20:42:21 -0500

    desafio06
    organização dos desafio no index.js
    e a criação do desafio 06

--- index.js
+++ index.js
@@ -103,14 +103,14 @@ console.log(chars)
 
 // Desafio: desenvolva uma calculadora para as 4 operações básicas usando const com os dados vindo de um formulário.
 
-
+_______________ desafio ________________
 function sum(){
     const formData = new FormData(document.querySelector('form'))
     const n1 = formData.get('n1')
     const n2 = formData.get('n2')
     console.log(n1+' '+n2)
 }
-
+______________ fim do desafio
 // object literal
 
 const product = {
@@ -156,6 +156,18 @@ console.log(dogObject)
 
 // Desafio 1: Armazene 5 dados numéricos em um vetor. Em seguida, copie para um objeto com as propriedades n1, n2, n3, n4 e n5. Utilize destructuring para copiar os valores para 5 variáveis. Construa um objeto JSON com os valores das variáveis.
 
+
+__________________ desafio fiz errado________________
+const vetor = [8,10,12,14,16]
+console.log(vetor)
+
+let [v1, v2, v3, v4, v5] = vetor
+console.log(v2)
+
+
+__________fim _________________________
+ 
+// desafio________
 const vectorChallenge = [77,45,3,8,123]
 
 const objectChallenge = {
@@ -171,6 +183,59 @@ const {n1, n2, n3, n4, n5} = objectChallenge
 console.log(JSON.stringify({n1,n2,n3,n4,n5}))
 
 // Desafio 2: Desenvolva uma calculadora que armazene em um objeto JSON o resultado das 4 operações básicas. Em outras palavras: crie um objeto calculadora, inicialize cada uma das operações da calculadora (que serão propriedades do objeto) com o resultado do processamento matemático vindo de duas variáveis e converta para um objeto JSON.
+
+
+__________desafio________________
+let vetJson = JSON.stringify(vetor)
+console.log(vetJson)
+
+const destruindoJson = `{
+
+    "n1": ${v1},
+    "n2": ${v2},
+    "n3": ${v3},
+    "n4": ${v4},
+    "n5": ${v5}
+
+}`
+console.log(destruindoJson)
+const jsonCalculadora = `{"numero01":8, "numero02":4}`
+console.log(jsonCalculadora)
+
+
+const numerosJson = JSON.parse(jsonCalculadora)
+console.log(numerosJson)
+
+const numeroA = numerosJson.numero01
+console.log(numeroA)
+
+const numeroB = numerosJson.numero02
+console.log(numeroB)
+
+//soma
+const soma = numeroA + numeroB
+
+
+//sub
+const sub = numeroA - numeroB
+
+//mult
+const mult = numeroA * numeroB
+
+//divid
+
+const div1 = numeroA / numeroB
+
+const div2 = numeroB / numeroA
+
+
+console.log("soma: ", soma)
+console.log("subtração: ", sub)
+console.log("multiplicação: ", mult)
+console.log("divição A por B: ", div1)
+console.log("divição B por A: ", div2)
+
+_____________fim do desafio___________
 
 const num1 = 10
 const num2 = 20
@@ -356,7 +421,7 @@ function out(){
 }
 console.log(out())
 
-
+// desafio ______________
 let varX = 10
 
     function sumXand5(){
@@ -367,7 +432,19 @@ let varX = 10
 // Desafio A+: Fazer esquema em que um código declara uma variável (let) e uma função faz a soma do valor da variável com um valor estático (5, por exemplo) como retorno. Outro código importa o primeiro e declara uma variável com o mesmo nome e outro valor. Em seguida, chama a função do primeiro código. Qual valor será exibido?
 // Utilizar o artigo disponível no endereço https://www.horadecodar.com.br/2020/08/13/como-incluir-um-arquivo-de-javascript-em-outro/
 
+
+__________desafio_________
+let varX = 10
+
+    function sumXand5(){
+        return varX+5
+    }
+    export {sumXand5, varX}
 /*
+
+
+_________fim_______
+
 
 // orientação a objetos
 class Product {
@@ -433,3 +510,25 @@ button.addEventListener('click',()=>(texts[3].style.backgroundcolor='blue'))
 
 ////////aula do dia 09-11
 
+
+/*Desafio: Crie um documento HTML com apenas um elemento ul, ou seja, uma lista não ordenada. Em seguida, crie um documento em JavaScript, utilize fetch (é uma API nativa do ES6 para requisições HTTP através de Promises) para pegar o conteúdo da página http://jsonplaceholder.typicode.com/users. Em seguida, dentro do fetch, utilize um then para converter o conteúdo da página para um objeto JSON e outro then para mapear o vetor com os dados do objeto JSON, puxando o nome e email de cada dado para a lista no HTML. Trate também o erro, quando houver.
+Inicie suas pesquisas para resolver o problema em https://www.devmedia.com.br/javascript-fetch/41206.*/
+
+fetch('http://jsonplaceholder.typicode.com/users')
+
+.then((response) => response.json()) // passando a reposta do json
+.then((data) => { //data e onde fica os dados brutos formato objeto
+    const name = data.map((user) => user.name) // mapeando os dados nome para jogar na variavel do tipo vetor
+    const email = data.map((user) => user.email)
+    console.log(name)
+    console.log(email)
+
+    const ul = document.querySelector('ul') // selecionando um objeto da pasta html
+    for(let i = 0; i < name.length; i++) { // percorrendo o vetor ate o fim dos elementos
+        const li = document.createElement('li') // criando o elementos e adicionado 
+        li.textContent = name[i]+' _ ' + email[i]
+        ul.appendChild(li)//adiciona um novo ul
+}
+
+
+})
--- node-day-01/scripts.js
+++ node-day-01/scripts.js
@@ -19,7 +19,6 @@ server.listen(port, hostname, () => {
 //callback
 
 const fs = require('fs')
-
 console.log('primeiro comaando')
 console.log('segunda comaando')
 console.log('terceira comaando')
@@ -54,6 +53,7 @@ jogar em um callback com 5000 (5s) para ver oque acontece
 
 oque esta acontecendo */
 
+/*
 function myFunction(){
 const data = new Date()
 const hora = data.getHours()
@@ -68,3 +68,107 @@ setTimeout(myFunction, 5000)
 setTimeout(myFunction, 5000)
 setInterval(myFunction, 5000)
 console.log("pedro antônio")
+*/
+//utulizando programação sincrona
+
+/*
+function sum(){
+const result = 1+1
+  if (result === 2){
+      setTimeout(sucesso, 5000)
+    }
+  else{
+    erro()
+  }
+function sucesso(){
+  console.log('Sucesso! A soama e 2')
+}
+
+
+function erro(){
+  console.log('erro a soma não da 2')
+}
+}
+
+sum()
+console.log("teste de exibição")
+*/
+
+/*
+//forma de callback
+function sum(callback, callbackError){
+  const result = 1+1
+    if (result === 2){
+        callback()
+      }
+    else{
+      callbackError()
+  }
+}
+  
+sum(
+    () =>(console.log('Sucesso! A soama e 2')),
+    () => (console.log('erro a soma não da 2'))
+
+  )
+console.log("teste de exibição")
+
+
+// promise 
+// then() (execução quando a promesa for cumprida)
+//resolve e reject (promessa cumprida e não cumpridas)
+
+const promise = new Promise((resolve, rejact) =>{
+  const result = 1+1
+    if (result === 2){
+        resolve('Sucesso! A soama e 2')
+      }
+    else{
+      rejact('erro a soma não da 2')
+    }
+})
+
+//promise.then((resultado)=>(console.log(resultado)))//quer receber oque vai acontecer quando der certo e quando der errado
+
+promise.then(
+  ((resultado)=>(console.log(resultado))),
+  ((erro)=>(console.log(erro)))
+)
+*/
+//voltando para callback
+/*
+const name = 'Paulo Sampaio'
+
+function whoIsTheBest(callback, callbackError){
+  if(name !='Paulo Sampaio'){
+    callbackError('Ta errado. Não tem como!')
+  }else{
+  //callback(`${name} e o melhor!`)
+  callback({
+    name: name,
+    message:`humildemente o melhor`
+  })
+  }
+}
+whoIsTheBest(
+  ((sucesso)=>(console.log(`{$sucess.name} e ${sucess.message}`))),
+  ((error)=>(console.log(error)))
+  )
+*/
+const name = 'Paulo Sampaio'
+
+  const promise = new Promise((resolve, rejact) =>{
+    if(name !='Paulo Sampaio'){
+      rejact('Ta errado. Não tem como!')
+    }else{
+    resolve({
+      name: name,
+      message:`humildemente o melhor`
+    })
+    }
+  })
+  
+promise.then(
+  ((sucesso)=>(console.log(`${sucesso.name} e ${sucesso.message}`))),
+  ((erro)=>(console.log(erro)))
+)
